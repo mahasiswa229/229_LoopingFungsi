b8efb181e4f0801c353bd4acc68199d5b21a3691
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-19 15:58:02 +0800

    bikin while

--- 2.cpp
+++ 2.cpp
@@ -2,3 +2,19 @@
 using namespace std;
 
 int main()
+{
+ int bilangan;
+
+ srand(time(0));
+
+ bilangan = rand() % 10;
+
+ while (bilangan%2)
+ {
+    cout << "bilangan " << bilangan << "adalah ganjil" << endl;
+    bilangan = rand() %10;
+ }
+
+ cout << "bilangan " << bilangan << " adalah genap" << endl;
+
+}
