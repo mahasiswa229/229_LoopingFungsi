72659776f220721e7abb72d0d9910ddc6a4e6ca9
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-19 15:36:34 +0800

    first commit

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i;
+    string nama[5]
+
+
+    for (i = 7 ; i > 0; i--)
+    {
+        cout << i << "." << "bantul" << endl;
+    }
+    
+    for (i =0; i < 5; i++)
+    {
+        cout << "masukan nama";
+        cin >> nama[i];
+    } 
+
+    cout << endl;
+
+
+}
