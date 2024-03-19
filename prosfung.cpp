50e1565911471beea89ea3a5e388ba894495be33
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-19 16:30:20 +0800

    masukin display

---
+++ prosfung.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+string nama;
+int nErspo,nMills,nHoka,nNike;
+int  hErspo = 100,hMills = 200,hHoka = 450,hNike =350;
+
+void input()
+{
+    cout << "jumlah Erspo =";
+    cin >> nErspo;
+
+    cout << "jumlah Mills =";
+    cin >> nMills;
+
+    cout << "jumlah Hoka =";
+    cin >> nHoka;
+
+    cout << "jumlah Nike =";
+    cin >> nNike;
+
+}
+
+int totalharga()
+{
+    return (nErspo*hErspo)+(nMills*hMills)+(nHoka*hHoka)+(nNike*hNike)
+}
+
+void display()
+{
+    cout <<endl;
+    cout << "===========" << endl ;
+    cout << endl;
+    cout << "jumlah erspo = " << nErspo
+    cout
+}
+
+int main()
+{
+
+}
