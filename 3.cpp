459f63bc5f7027ea715067990e2e862c79654195
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-19 16:07:27 +0800

    bikin do while done

--- 3.cpp
+++ 3.cpp
@@ -2,3 +2,16 @@
 using namespace std;
 
 int main()
+{   
+     string pilihan;
+
+    do
+    {
+        cout << "makan" << endl;
+        cout << "main game" << endl;
+        cout << "ibadah" << endl;
+
+        cout << "masih idup ga lu?(yes/no)";
+    } while (pilihan == "yes");
+    
+}
